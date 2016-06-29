436abfbc7936ad8c63b1d9e227514b86c1bd896f
Leonardo <Leonardo Torres>
2016-06-29 10:43:00 -0400

    refactor scripts/ directory to match content/

--- scripts/graphs/nodes_edges.py
+++ scripts/graphs/nodes_edges.py
@@ -1,19 +1,19 @@
 """
-scripts/directed.py
+scripts/nodes_edges.py
 ==============
 
-Code used for /content/directed.md.
+Code used for /content/nodes_edges.md.
 """
 
-import os
 import networkx as nx
+import os
 from matplotlib.pylab import plt
 
 
 def example_pic():
     """Generate the example graph picture."""
     # create graph from edge list
-    graph = nx.DiGraph([(0, 1), (0, 2), (1, 3), (3, 0), (0, 3)])
+    graph = nx.Graph([(0, 1), (0, 2), (1, 3), (3, 0)])
 
     # positions for all nodes
     pos = nx.spring_layout(graph)
@@ -27,12 +27,11 @@ def example_pic():
 
     # draw all of the things!
     nx.draw_networkx_nodes(graph, pos, nodelist=[0, 1, 2, 3], node_color='r')
-    nx.draw_networkx_edges(graph, pos, width=1.0, alpha=0.5, arrows=True)
+    nx.draw_networkx_edges(graph, pos, width=1.0, alpha=0.5)
     nx.draw_networkx_labels(graph, pos, labels, font_size=16)
 
     # place the file where it belongs
-    path = os.path.split(os.getcwd())[0]
-    path = os.path.join(path, "content/images", "directed_example.png")
+    path = os.path.join(os.environ['ERDOS_PATH'], "content/images", "nodes_edges_example.png")
     plt.savefig(path)
 
     # plt.show()
