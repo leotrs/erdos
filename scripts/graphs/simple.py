3e3da7c07294aeb0455f509022176889ce44cc13
Leonardo <Leonardo Torres>
2016-07-01 00:52:35 -0400

    fix unclosed tag.

--- scripts/graphs/simple.py
+++ scripts/graphs/simple.py
@@ -5,36 +5,39 @@ scripts/simple.py
 Code used for /content/simple.md.
 """
 
+"""
+simple.py
+=========
+
+This script uses the .dot graph file format, because networkx doesn't
+support drawing multigraphs. This script uses the 'neato' command, which
+comes with pygraphviz, which needs graphviz to be installed first.
+
+  apt-get install graphviz
+  pip install pygraphviz
+
+"""
+
+
 import os
 import networkx as nx
-from matplotlib.pylab import plt
 
 
 def example_pic():
     """Generate the example graph picture."""
-    # create graph from edge list
-    graph = nx.MultiGraph([(0, 1), (0, 2), (0, 3), (1, 3), (0, 3)])
+    path = os.path.join(os.environ['ERDOS_PATH'], "content/images")
+    command = 'neato -T png {0} > {1}'
 
-    # positions for all nodes
-    pos = nx.spring_layout(graph)
+    graph = nx.MultiGraph([(0, 1), (0, 2), (0, 3), (1, 3), (0, 3), (1, 1)])
+    tmp_path = '/tmp/simple1.dot'
+    nx.drawing.nx_agraph.write_dot(graph, tmp_path)
+    os.system(command.format(tmp_path, os.path.join(path, 'simple1.png')))
 
-    # each node is labaled by its own name
-    labels = {node: str(node) for node in graph.node.keys()}
+    graph = nx.Graph([(0, 1), (0, 2), (0, 3), (1, 3)])
+    tmp_path = '/tmp/simple2.dot'
+    nx.drawing.nx_agraph.write_dot(graph, tmp_path)
+    os.system(command.format(tmp_path, os.path.join(path, 'simple2.png')))
 
-    # configure the image
-    plt.figure(figsize=(2, 2))
-    plt.axis('off')
-
-    # draw all of the things!
-    nx.draw_networkx_nodes(graph, pos, nodelist=[0, 1, 2, 3], node_color='r')
-    nx.draw_networkx_edges(graph, pos, width=1.0, alpha=0.5, arrows=True)
-    nx.draw_networkx_labels(graph, pos, labels, font_size=16)
-
-    # place the file where it belongs
-    path = os.path.join(os.environ['ERDOS_PATH'], "content/images", "simple.png")
-    # plt.savefig(path)
-
-    plt.show()
 
 if __name__ == '__main__':
     example_pic()
