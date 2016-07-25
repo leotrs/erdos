91023fb1ed8cef0fdfdea203fdafe9517e1c108b
Leonardo <Leonardo Torres>
2016-07-25 15:27:01 -0500

    add a script that generates a skeleton file for challenge pages, using jinja.

---
+++ scripts/templates/README.md
@@ -0,0 +1,16 @@
+# erdos/templates
+
+This folder contains resources to automatically generate a new article.
+
+Just run the `genarticle.py` script from the top level folder, and pass it
+the path of the new article:
+
+```
+erdos/templates $ python scripts/templates/genarticle.py path_to_article
+```
+
+`path_to_article` must be relative to the `erdos/content` directory.
+
+`genarticle.py` will use `jinja2` and the templates in this directory to
+generate a skeleton of the file, complete with date and !INCLUDE directives
+already there.
---
+++ scripts/templates/genarticle.py
@@ -0,0 +1,56 @@
+"""
+genarticle.py
+-------------
+
+Generate the skeleton of an erdos article by using jinja2 templates.
+
+"""
+
+import os
+import argparse
+import subprocess
+from collections import defaultdict
+from jinja2 import Environment, FileSystemLoader
+
+
+ROOT = '/home/leo/code/erdos/'
+CONTENT_DIR = os.path.join(ROOT, 'content/')
+TEMPLATES_DIR = os.path.join(ROOT, 'scripts/templates/')
+TEMPLATE_FILE = 'article.mdpp'   # must be realteive to TEMPLATES_DIR
+
+
+def make_vars(**kwargs):
+    """Returns a dict with var=value pairs, to be passed to the template."""
+    vars_dict = defaultdict(str)
+    vars_dict.update(kwargs)
+
+    vars_dict['date'] = subprocess.getoutput('date "+%x"')
+
+    md5 = subprocess.getoutput('date | md5sum | cut -c1-8')
+    slug = vars_dict['summary'].lower().replace(' ', '-')
+    vars_dict['disqus_id'] = md5 + '-' + slug
+
+    return vars_dict
+
+
+def render(**kwargs):
+    """Creates the appropriate jinja2 objects and renders the template."""
+    env = Environment(loader=FileSystemLoader(TEMPLATES_DIR))
+    template = env.get_template(TEMPLATE_FILE)
+
+    return template.render(**make_vars(**kwargs))
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('path', help='the output file for the new article - '
+                        'must be relative to erdos/content directory')
+    parser.add_argument("-t", "--title", help="specify article title")
+    parser.add_argument("-c", "--category", help="specify article category")
+    parser.add_argument("-s", "--summary", help="specify article summary")
+    args = parser.parse_args()
+
+    with open(os.path.join(CONTENT_DIR, args.path), 'w+') as outfile:
+        text = render(**args.__dict__)
+        outfile.write(text)
