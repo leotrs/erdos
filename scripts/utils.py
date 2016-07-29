fd53fe0b6f4c43e9e573283cdc481cfe16cb48a7
Leonardo <Leonardo Torres>
2016-07-28 19:54:11 -0500

    feat: now labels are optional.

--- scripts/utils.py
+++ scripts/utils.py
@@ -14,7 +14,8 @@ from matplotlib.pylab import plt
 LAYOUT_DICT = {'spring': nx.spring_layout, 'shell': nx.shell_layout}
 
 
-def example_image(graph, filename, layout='spring', show=False):
+def example_image(graph, filename, layout='spring', edge_labels=False,
+                  node_labels=False, show=False):
     """Generates example image with graph.
 
     Uses nx.draw_networkx_* methods, and matplotlib to draw and save the
@@ -24,9 +25,6 @@ def example_image(graph, filename, layout='spring', show=False):
     # positions for all nodes
     pos = LAYOUT_DICT[layout](graph)
 
-    # each node is labaled by its own name
-    labels = {node: str(node) for node in graph.nodes()}
-
     # configure the image
     plt.figure(figsize=(2, 2))
     plt.axis('off')
@@ -34,7 +32,14 @@ def example_image(graph, filename, layout='spring', show=False):
     # draw all of the things!
     nx.draw_networkx_nodes(graph, pos, nodelist=graph.nodes(), node_color='r')
     nx.draw_networkx_edges(graph, pos, width=1.0, alpha=0.5, arrows=True)
-    nx.draw_networkx_labels(graph, pos, labels, font_size=16)
+
+    if node_labels:
+        nlabels = {node: str(node) for node in graph.nodes()}
+        nx.draw_networkx_labels(graph, pos, nlabels, font_size=16)
+
+    if edge_labels:
+        elabels = {edge: str(idx) for idx, edge in enumerate(graph.edges())}
+        nx.draw_networkx_edge_labels(graph, pos, elabels)
 
     # place the file where it belongs
     path = os.path.join(os.environ['ERDOS_PATH'], "content/images", filename)
