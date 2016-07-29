d7fbbfb7131a87e115c10179ebcf7b3d9fbcd121
leotrs <dleonardotn@gmail.com>
2016-07-28 19:48:27 -0500

    update to use new utils.

--- scripts/reprs/adjmatrix.py
+++ scripts/reprs/adjmatrix.py
@@ -1,41 +1,24 @@
 """
-scripts/adjmatrix.py
-====================
+adjmatrix.py
+------------
 
-Code used for /content/adjmatrix.md.
+Code used for reprs/adjmatrix.md.
 
 """
 
-import os
 import networkx as nx
-from matplotlib.pylab import plt
+
+from os import path, environ
+import sys
+sys.path.append(path.join(environ['ERDOS_PATH'], 'scripts/'))
+import utils
 
 
 def example_pic():
     """Generate the example graph picture."""
     # create graph from edge list
     graph = nx.Graph([(0, 1), (0, 2), (1, 3), (3, 0)])
-
-    # positions for all nodes
-    pos = nx.spring_layout(graph)
-
-    # each node is labaled by its own name
-    labels = {node: str(node) for node in graph.node.keys()}
-
-    # configure the image
-    plt.figure(figsize=(2, 2))
-    plt.axis('off')
-
-    # draw all of the things!
-    nx.draw_networkx_nodes(graph, pos, nodelist=[0, 1, 2, 3], node_color='r')
-    nx.draw_networkx_edges(graph, pos, width=1.0, alpha=0.5, arrows=True)
-    nx.draw_networkx_labels(graph, pos, labels, font_size=16)
-
-    # place the file where it belongs
-    path = os.path.join(os.environ['ERDOS_PATH'], "content/images", "adjmatrix.png")
-    plt.savefig(path)
-
-    # plt.show()
+    utils.example_image(graph, "adjmatrix.png")
 
 
 if __name__ == '__main__':
